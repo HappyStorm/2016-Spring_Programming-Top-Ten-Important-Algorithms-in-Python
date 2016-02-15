1467169dffeb97cb6668a6c09aea6074c77336f9
Wu Cheng-Wei <HaleyWu@WudeMacBook-Pro.local>
2016-02-15 15:08:45 +0800

    ver1.0

--- Sudoku.py
+++ Sudoku.py
@@ -5,7 +5,7 @@
 # 
 # 2016-2-15 @ Delta-107 National Tsing Hua University 
 
-# In[58]:
+# In[64]:
 
 import numpy as np
 board = np.array([[0,3,0,9,8,7,0,0,6],
@@ -19,18 +19,18 @@ board = np.array([[0,3,0,9,8,7,0,0,6],
                   [5,0,0,8,7,3,0,4,0]])
 
 
-# In[59]:
+# In[65]:
 
 def sudoku_solver(board):
     '''Sudoku Solver by CWWU'''
     stack = [(0,board)]
-    while len(stack) != 0:
+    while stack:
         (curr_depth, curr_board) = stack.pop()
         if curr_depth == 81:
             return curr_board
         else:
             row,col = curr_depth // 9, curr_depth % 9
-            if curr_board[row][col] != 0:
+            if curr_board[row, col]:
                 stack.append((curr_depth+1, curr_board.copy()))
             else:
                 moves = ({1,2,3,4,5,6,7,8,9} - 
@@ -45,7 +45,7 @@ def sudoku_solver(board):
     return None
 
 
-# In[60]:
+# In[66]:
 
 sudoku_solver(board)
 
